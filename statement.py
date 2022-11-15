685601d2b582930247f4c90dc51bec6121e1d6a3
sas <sasbergson@gmail.com>
2022-11-15 15:39:55 +0100

    created statement recognizer module
    basic statement recognizer for reading natural english language statements

---
+++ statement.py
@@ -0,0 +1,17 @@
+import os
+import re
+
+class statement_tokenizer:
+    
+    def __init__(self):
+        self._pattern='r"[A-Z]+[a-z]*\s\."'
+        self._regex = re.compile(self._pattern)
+        self._tokens=[]
+        
+    def get_tokens(self, text) -> list:
+        self._tokens= self._regex.split(text)
+        return self._tokens
+    
+    def __str__(self) -> str:
+        for s in self._tokens:
+          print(f"statement -> {s}")
