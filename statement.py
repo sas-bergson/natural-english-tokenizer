9578daf38acb1af89cb2c358c10f718d35d17c16
mbohbless <mbohblesspearl@gmail.com>
2022-11-21 20:59:08 +0100

    undestanding documentation generation

--- statement.py
+++ statement.py
@@ -1,17 +1,25 @@
+
 import os
 import re
 
+"""! @brief Example Python program with Doxygen style comments."""
+
+
 class statement_tokenizer:
-    
     def __init__(self):
-        self._pattern='r"[A-Z]+[a-z]*\s\."'
+        """
+        This method is the constructor. where all the regex are defined and the word that the user or the calling file will use to call the method."""
+        self._pattern = 'r"[A-Z]+[a-z]*\s\."'
         self._regex = re.compile(self._pattern)
-        self._tokens=[]
-        
+        self._tokens = []
+
     def get_tokens(self, text) -> list:
-        self._tokens= self._regex.split(text)
+        """
+        This method is responsible for the splitting of the individual strings into the required tokens which
+        takes  text (string) the text that is to be split into tokens. and returns a list of tokens."""
+        self._tokens = self._regex.split(text)
         return self._tokens
-    
+
     def __str__(self) -> str:
         for s in self._tokens:
-          print(f"statement -> {s}")
+            print(f"statement -> {s}")
