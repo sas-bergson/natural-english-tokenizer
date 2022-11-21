f699a171c724bfaf6ea06c27933b17b59f1a52af
mbohbless <mbohblesspearl@gmail.com>
2022-11-21 20:59:18 +0100

    undestanding documentation generation

---
+++ docs/html/search/all_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['statement_0',['statement',['../namespacestatement.html',1,'']]],
+  ['statement_2epy_1',['statement.py',['../statement_8py.html',1,'']]],
+  ['statement_5ftokenizer_2',['statement_tokenizer',['../classstatement_1_1statement__tokenizer.html',1,'statement']]]
+];
---
+++ docs/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['get_5ftokens_0',['get_tokens',['../classstatement_1_1statement__tokenizer.html#ac857ab8a63128b061b40164fb76c8bd2',1,'statement::statement_tokenizer']]]
+];
