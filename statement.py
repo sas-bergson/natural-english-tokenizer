636fe6a34d4da169ed4c5f547d230d8127af9f46
TatchumNono <Tatchumnono@gmail.com>
2022-11-22 09:56:42 +0100

    Created main implementation file and sentence tokenizer

--- statement.py
+++ statement.py
@@ -4,14 +4,27 @@ import re
 class statement_tokenizer:
     
     def __init__(self):
+        #self._pattern='r"[A-Z]+[a-z]*\s\."'
         self._pattern='r"[A-Z]+[a-z]*\s\."'
         self._regex = re.compile(self._pattern)
         self._tokens=[]
         
     def get_tokens(self, text) -> list:
-        self._tokens= self._regex.split(text)
+        #self._tokens= self._regex.split(text)
+        self._tokens = text.split()
         return self._tokens
     
     def __str__(self) -> str:
         for s in self._tokens:
           print(f"statement -> {s}")
+    
+    
+          
+test = statement_tokenizer()
+        
+def pro():
+    test.get_tokens("Hi my name is Yan")
+    test.__str__()
+    #print(test._tokens)
+
+pro()
