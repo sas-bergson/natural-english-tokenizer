b101f1010d1f6761339a5a90a751c626368f35e4
mbohbless <mbohblesspearl@gmail.com>
2022-11-26 23:38:08 +0100

    creation of default word tokenizer

--- statement.py
+++ statement.py
@@ -11,6 +11,7 @@ class statement_tokenizer:
         This method is the constructor. where all the regex are defined and the word that the user or the calling file will use to call the method."""
         self._pattern = r"[A-Z]+[a-z]*\s\."
         self._sentence_pattern = r'([A-Z][^\.!?]*[\.!?])'
+        self._word_pattern = r'\w+'
         self._regex = re.compile(self._sentence_pattern)
         self._tokens = []
 
@@ -18,7 +19,7 @@ class statement_tokenizer:
         """
         This method is responsible for the splitting of the individual strings into the required tokens which
         takes  text (string) the text that is to be split into tokens. and returns a list of tokens."""
-        self._tokens = self.performSentenceSplit(text)
+        self._tokens = self.peformSentenceSplit(text)
         return self._tokens
 
     def __str__(self) -> str:
@@ -35,7 +36,7 @@ class statement_tokenizer:
         for s in self._tokens:
             print(f"statement -> {s}")
 
-    def performSentenceSplit(self, text):
+    def peformSentenceSplit(self, text):
         """This function takes part in tokenization of whole text blocks to aid the word tokenizer to be able to identifty tokens 
         based on specific word. This is a part which makes the whole system modular making it possible for the sentence to be handled as 
         blocks. this will also give the possibility to thet count of the number of sentences in 
@@ -43,7 +44,16 @@ class statement_tokenizer:
         formatter = re.compile(self._sentence_pattern, re.M)
         return formatter.findall(text)
 
-    def fmtcharRecog(self, text):
+    def performWordSplit(self, text):
+        """This function takes part in tokenization of whole text blocks to aid the word tokenizer to be able to identifty tokens 
+        based on specific word. This is a part which makes the whole system modular making it possible for the sentence to be handled as 
+        blocks. this will also give the possibility to thet count of the number of sentences in 
+        """
+        formatter = re.compile(self._word_pattern, re.M)
+        return formatter.findall(text)
+
+
+    def retainAllTokens(self, text):
         """
         this fuction is tp prevent the elimination of special characters to avoid elimination during text splitting 
         this will be especially important what there will to be identification of known patters.
@@ -57,7 +67,7 @@ class statement_tokenizer:
                 new_text = new_text+text[i]+" "
             else:
                 new_text = new_text+text[i]
-        return new_text+2
+        return new_text
 
 
 if __name__ == "__main__":
@@ -65,5 +75,5 @@ if __name__ == "__main__":
 
     checker = data.get_tokens(
         """ Living without computers today is close to an impossibility. As our reliance on computers and computer-controlled technologies grows the computer has evolved from a luxury item to a necessity.""")
-
-    print(checker)
+    for i in checker:
+        print(data.performWordSplit(i))
