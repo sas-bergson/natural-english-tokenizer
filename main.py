de9b8098c3f55d44e7d1acb742f30916da3530f2
Sybillemaeva <97405075+Sybillemaeva@users.noreply.github.com>
2022-11-28 22:39:35 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import statement as st
+
+if __name__ == '__main__':
+    st = st.StatementTokenizer()
+    st.tokenizer('Sybille is a good programmer. She studies hard.')
--- statement.py
+++ statement.py
@@ -1,17 +1,172 @@
-import os
 import re
+import json
 
-class statement_tokenizer:
-    
+class StatementTokenizer:
+    """!This is the statement tokenizer class.
+    """
+   
     def __init__(self):
-        self._pattern='r"[A-Z]+[a-z]*\s\."'
-        self._regex = re.compile(self._pattern)
-        self._tokens=[]
+        """!This is the StatementTokenizer class initializer."""
         
-    def get_tokens(self, text) -> list:
-        self._tokens= self._regex.split(text)
-        return self._tokens
-    
-    def __str__(self) -> str:
-        for s in self._tokens:
-          print(f"statement -> {s}")
+        # Regular expression for noun
+        self._noun_regex = re.compile(r"[a-zA-Z][0-9]*")
+        # Regular expression for full stop
+        self._full_stop_regex = re.compile(r"\.")
+        # Regular expression for white space
+        self._white_space_regex = re.compile(r"\s")
+        # Array of tokens
+        self._tokens = []
+
+        # Load dictionary which contains the 100 first words beginning with letter N 
+        file = open('dictionary.json')
+        json_data = json.load(file)
+        print(type(json_data))
+        self.dictionary = []
+        for value in json_data:
+            self.dictionary.append({
+                'type': value['type'],
+                'value': value['value']
+            })
+        # print(self.dictionary)
+        file.close()
+
+
+    # Search if word present in dictionary
+    def find_word(self, value: str) -> object:
+
+        for x in self.dictionary:
+            if x['value'] == value:
+                return x
+
+
+    # Defines the pattern for the list
+    def tokenize_pattern(self, word_type: str, pattern, expression: str, current: int) -> list:
+        
+        char = expression[current]
+        consumed_chars = 0
+        if re.match(pattern, char):
+            value = ''
+            while char and re.match(pattern, char):
+                value += char
+                consumed_chars += 1
+                try:
+                    char = expression[current + consumed_chars]
+                except:
+                    char = None
+
+            if word_type:
+                return [consumed_chars, {'type': word_type, 'value': value}]
+            else:
+                word = self.find_word(value.lower())
+                if word:
+                    return [consumed_chars, {'type': word['type'], 'value': word['value']}]
+                else:
+                    return [consumed_chars, {'type': 'unknown type', 'value': value}]
+        return [0, None]
+
+
+    # Tokenize sentence
+    @staticmethod
+    def tokenize_sentence(expression: str, current: int) -> list:
+
+        # Check if expression begin with uppercase letter
+        if re.match(re.compile(r"[A-Z]"), expression[current]):
+            value = ''
+            consumed_chars = 0
+            char = expression[current + consumed_chars]
+            while char != '.':
+                if char is None:
+                    raise Exception("Unterminated sentence")
+                value += char
+                consumed_chars += 1
+                try:
+                    char = expression[current + consumed_chars]
+                except:
+                    char = None
+
+            # If end of expression, add full stop at the end of token
+            if char == '.':
+                value += char
+
+            return [(consumed_chars + 1), {'type': 'sentence', 'value': value}]
+
+        return [0, None]
+
+
+    def get_sentences(self, statement: str) -> list[str]:
+        
+        sentences = []
+        current = 0
+        # Load mini tokenizers
+        tokenizers = [self.skip_white_space, StatementTokenizer.tokenize_sentence]
+
+        while current < len(statement):
+            tokenized = False
+            # Loop in all mini tokenizers in order to get sentences
+            for function in tokenizers:
+                if tokenized:
+                    break
+                result = function(statement, current)
+                if result[0] != 0:
+                    tokenized = True
+                    current += result[0]
+                if result[1]:
+                    sentences.append(result[1]['value'])
+        return sentences
+
+
+    def tokenizer(self, statement: str):
+        
+        sentences = self.get_sentences(statement)
+        # Loop over the array of sentences and tokenize each of them
+        for sentence in sentences:
+            current = 0
+            # Load mini tokenizers
+            tokenizers = [self.skip_white_space, self.tokenize_string, self.tokenize_full_stop]
+            while current < len(sentence):
+                tokenized = False
+                char = sentence[current]
+                # Loop in all mini tokenizers in order to categorize found tokens
+                for function in tokenizers:
+                    if tokenized:
+                        break
+                    result = function(sentence, current)
+                    if result[0] != 0:
+                        tokenized = True
+                        current += result[0]
+                    if result[1]:
+                        self._tokens.append(result[1])
+
+                # If not reach at the end of the sentence and unknown char
+                if not tokenized:
+                    raise Exception("I don't know what this character is: ", char)
+                # If reach at the end of the sentence
+                elif char == '.':
+                    print('sentence : "', sentence, '" ', 'successfully tokenized')
+        print('tokens:', self._tokens)
+
+
+
+    # Tokenize string
+    def tokenize_string(self, expression: str, current: int) -> list:
+
+        return self.tokenize_pattern(None, self._noun_regex, expression, current)
+
+
+    # Tokenize full stop
+    def tokenize_full_stop(self, expression: str, current: int) -> list:
+       
+        return self.tokenize_pattern("full stop", self._full_stop_regex, expression, current)
+
+
+    # Tokenize white space
+    def skip_white_space(self, expression: str, current: int) -> list:
+        
+        if re.match(self._white_space_regex, expression[current]):
+            return [1, None]
+        else:
+            return [0, None]
+
+   
+
+          
