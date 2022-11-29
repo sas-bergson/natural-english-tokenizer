62411b2494ca26a409764858f1d3626223676f41
Emmanueljr07 <116388126+Emmanueljr07@users.noreply.github.com>
2022-11-29 14:03:36 +0100

    checking the sentence

---
+++ sentence.py
@@ -0,0 +1,20 @@
+import regex as re
+
+class Sentence:
+    
+    f = open('./assets/sample.txt', 'r')
+    sample = f.read()
+    f.close()
+    
+    def sentence_check(text):
+        pattern = re.compile('[A-Z][^\.!?]*[!?\.]')
+        if re.match(pattern, text):
+            text
+            print(text)
+            return text
+        else:
+            print("Invalid")
+        
+    
+    sentence_check(sample)
+    
