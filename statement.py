6a4049e10a56f358aff6fb1a259e902a0440653b
Paradox369 <george.ketcha@gmail.com>
2022-11-29 15:01:13 +0100

    minor updates to statement.py

--- statement.py
+++ statement.py
@@ -17,9 +17,6 @@ class statement_tokenizer:
         for s in self._tokens:
             print(f"statement -> {s}")
 
-
-
-
 # splitting into phrases
     def sentence_scanner(self, text : str):
         temp_phrase = ""
@@ -43,15 +40,14 @@ class statement_tokenizer:
 
 # splitting into words
     def word_scanner(self, array_of_phrases: []):
-        tokens = []
         word_count = 0
         for phrase in array_of_phrases:
             temp_words = phrase.split()
             for word in temp_words:
-                tokens.append(word.lower())
+                self._tokens.append(word.lower())
                 temp_words = []
-        print("word count: ", tokens.__len__())
-        return tokens
+        print("word count: ", self._tokens.__len__())
+        return self._tokens
 
 
 
@@ -59,10 +55,18 @@ class statement_tokenizer:
 
 
 
-text = "It was their first date and she had been looking forward to it the entire week. She had her eyes on him for months, and it had taken a convoluted scheme with several friends to make it happen, but he'd finally taken the hint and asked her out. After all the time and effort she'd invested into it, she never thought that it would be anything but wonderful. It goes without saying that things didn't work out quite as she expected. Matt told her to reach for the stars, but Veronica thought it was the most ridiculous advice she'd ever received. Sure, it had been well-meaning when he said it, but she didn't understand why anyone would want to suggest something that would literally kill you if you actually managed to achieve it. Dragons don't exist they said. They are the stuff of legend and people's imagination. Greg would have agreed with this assessment without a second thought 24 hours ago. But now that there was a dragon staring directly into his eyes, he questioned everything that he had been told."
+text = "It was their first date and she had been looking forward to it the entire week. She had her eyes on him for months, and it had taken a " \
+       "convoluted scheme with several friends to make it happen, but he'd finally taken the hint and asked her out. After all the time and effort " \
+       "she'd invested into it, she never thought that it would be anything but wonderful. It goes without saying that things didn't work out quite " \
+       "as she expected. Matt told her to reach for the stars, but Veronica thought it was the most ridiculous advice she'd ever received. Sure, it " \
+       "had been well-meaning when he said it, but she didn't understand why anyone would want to suggest something that would literally kill you " \
+       "if you actually managed to achieve it. Dragons don't exist they said. They are the stuff of legend and people's imagination. Greg would " \
+       "have agreed with this assessment without a second thought 24 hours ago. But now that there was a dragon staring directly into his eyes, " \
+       "he questioned everything that he had been told."
 
+text1 = "This is a test designed to verify the behaviour of the tokenizer. If it succeeds, we will move to the design of a file scanner."
 # printing array of phrases
-obj = statement_tokenizer().sentence_scanner(text)
+obj = statement_tokenizer().sentence_scanner(text1)
 #print(obj)
 
 # more comprehensive presentation of array of phrases
