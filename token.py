f4963a2753105ae4501c3f61ba4049b79cbf8d42
wallang-A-M-P <97224559+wallang-A-M-P@users.noreply.github.com>
2022-12-04 13:58:49 +0100

    Updated the sentence breaker

---
+++ token.py
@@ -0,0 +1,6 @@
+def sentences():
+    tokens = text.split(' ')
+    print('tokens => ')
+    print(tokens)
+
+text = 'I am a realest nig in the chor'
--- tokeniser/main.js
+++ tokeniser/main.js
@@ -1,8 +1,8 @@
-text = "the quick brown fox jumped over the lazy dog. The Main miners of the world. An anthilled"
+text = "The quick brown fox jumped over the lazy dog. The Main miners of the world. An anthilled"
 //divide into sentences
 var check_sentence = (text)=>{
     split_sentence = text.split('.')
-    console.log('sentences:');
+    console.log(`sentences(${split_sentence.length}):`);
     for(i = 0; i < split_sentence.length; i++){
         console.log('  -> '+split_sentence[i]);
     }
