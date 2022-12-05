f73ee8c2d8800979b4075a081955d4e8aa5165f5
Kamadeusamira <97232094+Kamadeusamira@users.noreply.github.com>
2022-12-05 16:02:28 -0500

    noun copmmit -6

--- parts_speech.py
+++ parts_speech.py
@@ -5,7 +5,7 @@ noun = [
     "beef","battery","barn","bone","back","butter","bread","beeper","bowels","bacon","blaze","blowup","blackener","blackhead",
     "brisket","barberry","blueberries","bus stop","brow","bale","birthplace","berlin","bubbles","buffer","bucket","bleachers",
     "bike","book","bar","breeze","best","backing","block","blackpool","border","bracelet","bobbery","bunkup","band","bucking",
-    
+    "bag","bootes","bowtie","bridle","bettery","backhoe","bluebill","barnacle","bongo","beef","bicycle","bulldog","bluebill",
    ]
     
 # list of verbs starting with the letter "b"
