37027ecb9bd37440ade74a5ec5f10ea37bf6b39c
Kamadeusamira <97232094+Kamadeusamira@users.noreply.github.com>
2022-12-05 15:54:58 -0500

    noun commit -3

--- parts_speech.py
+++ parts_speech.py
@@ -1,7 +1,10 @@
 # list of nouns starting with b
 noun = [
     "bachelor","backbone","balance","brightness","benefit","being","best","booster","bachelor","babe","black","board",
-    "boarder","broker","bestseller","beverage","bean","bail","beast","batch","behind"]
+    "boarder","broker","bestseller","beverage","bean","bail","beast","batch","behind","bracket","brace","builder","beefalo",
+    "beef","battery","barn","bone","back","butter","bread","beeper","bowels","bacon","blaze","blowup","blackener","blackhead",
+   ]
+    
 # list of verbs starting with the letter "b"
 verbs = [
     "bake"],
