eeb8db7fe55a293a4fdb2926a4e5578da90bbccb
NjiahDrick <119471911+NjiahDrick@users.noreply.github.com>
2022-12-05 19:30:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from scanner.tokenizer import Tokenizer
 if __name__ == "__main__":
     t = Tokenizer()
     text = 'This is a test designed to verify the behaviour\
-        of the tokenizer. If it succeeds, we will move to the design of a file scanner.'
+    
    
  
