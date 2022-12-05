2e704cc6ae8a23ea6342a3785b04e21791de54cb
mbohbless <mbohblesspearl@gmail.com>
2022-12-05 21:36:11 +0100

    creation of variables

---
+++ parts_speech.py
@@ -0,0 +1,20 @@
+# list of nouns starting with b
+noun = [
+    "bachelor",
+]
+# list of verbs starting with the letter "b"
+verbs = [
+    "bake"],
+# list of adjectives starting with the letter "b"
+adjectives = [
+    "bad",
+]
+# list of adverbs starting with the letter "b"
+adverbs = [
+    "badly",
+]
+# list of pronouns starting with the letter "b"
+pronouns = [
+    "both",
+
+]
