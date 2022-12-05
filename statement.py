a222e472d5352cb40599c5621bdc554ae896bbe3
NjiahDrick <119471911+NjiahDrick@users.noreply.github.com>
2022-12-05 19:23:35 +0100

    Update statement.py

--- statement.py
+++ statement.py
@@ -6,7 +6,7 @@ class statement_tokenizer:
     def __init__(self):
         self._pattern='r"[A-Z]+[a-z]*\s\."'
         self._regex = re.compile(self._pattern)
-        self._tokens=[]
+       
         
    
     
