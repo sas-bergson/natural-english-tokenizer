176cd3a45492474c17c603895a08fd0ab5667add
Blueoteq237 <117952211+Blueoteq237@users.noreply.github.com>
2022-12-06 11:46:28 +0100

    Create Tokinizer.py

---
+++ Tokinizer.py
@@ -0,0 +1,40 @@
+#This function splits a paragraph into sentences and stores them as a list
+# which it then returns a list of sentences.
+def sentence_split(text):
+    sentences = text.split('.')
+    count = 0
+    
+    #This function removes the empty values from the list.
+    sentences.remove('')
+    print('sentences('+str(len(sentences))+') : ')
+    for sentence in sentences:
+        if (sentence != ''):
+            print(' -> '+sentence)
+            sentence.split(',')
+        else:
+            sentences.remove(sentence)
+    return sentences
+
+
+#This function splits sentences into words and stores them as a list.
+def word_split(text):
+    sentences_list = []
+    for sentence in text:
+        words = sentence.split(' ')
+        if (words.count('')>=1):
+            words.remove('')
+        print('Sentence'+':\n'+ sentence)
+        print('Words:'+'('+str(len(words))+')')
+        for word in words:
+            #Removing Commas from words
+            #if (word.count(',')>=1):
+             #   word.remove(',')
+            print('-> ' + word)
+        sentences_list.append(words)
+    return sentences_list
+
+
+text = 'I beleive in Christ. The choir is made up of youths, parents, and grandparents. The fear of the Lord is my salvation.'
+test = sentence_split(text)
+box = word_split(test)
+print(box)
