e5fb130ef9bab44947d9a6fcd086c1ac5e106500
Abdoul <azisabdoul75@gmail.com>
2022-12-06 01:54:45 +0100

    setting up sentence structure

--- core/structure.py
+++ core/structure.py
@@ -30,3 +30,40 @@ class Document():
             self._index+=1
             return result
         raise StopIteration
+    
+class Sentence():
+      
+    def __init__(self, start_position, end_position, raw_document_reference):
+       
+        self.start_pos = int(start_position)
+        self.end_pos = int(end_position)
+        self._document_string = raw_document_reference
+        self.next_sentence = None
+        self.previous_sentence = None
+        #self.tokens = tokenize(self._document_string[self.start_pos:self.end_pos])
+        self._index = 0
+
+    def get(self):
+        return self._document_string[self.start_pos:self.end_pos]
+
+    def __getitem__(self, key):
+        return self.tokens[key]
+
+    def __repr__(self):
+        return self.get()
+
+    def __str__(self):
+        return self.get()
+
+    def __eq__(self, other):
+        return self.get() == other
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index < len(self.tokens):
+            result = self.tokens[self._index]
+            self._index+=1
+            return result
+        raise StopIteration
