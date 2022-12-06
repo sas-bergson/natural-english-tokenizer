7a12ea90e9c892471d0abd55f7a332fb98dddbc2
PRIME689 <misterprime74@gmail.com>
2022-12-06 14:40:31 +0100

    corrections

---
+++ LexerError.py
@@ -0,0 +1,11 @@
+import re
+import sys
+
+
+class LexerError(Exception):
+    """ Lexer error exception.
+        pos:
+            Position in the input line where the error occurred.
+    """
+    def __init__(self, pos):
+        self.pos = pos
--- main.py
+++ main.py
@@ -10,6 +10,8 @@
 #-------------------------------------------------------------------------------
 import re
 import sys
+import Token
+import LexerError
 
 
 class statement_tokenizer:
