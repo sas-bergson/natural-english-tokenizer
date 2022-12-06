3c5e23e3503ed7d96993f69a4ba5c00de2876a7f
wallang-A-M-P <97224559+wallang-A-M-P@users.noreply.github.com>
2022-12-06 14:52:54 +0100

    Further Modularisation

--- Tokenizer_python/token.py
+++ Tokenizer_python/token.py
@@ -1,45 +1,52 @@
 #This function splits a paragraph into sentences and stores them as a list
 # which it then returns a list of sentences.
-def sentence_split(text):
-    sentences = text.split('.')
-    count = 0
-       
-    sentences.remove('') #This line removes the empty values from the list.
-    print(text)
-    print('\nsentences('+str(len(sentences))+') : ')
-    for sentence in sentences:
-        if (sentence != ''):
-            print(' -> '+sentence)
-            sentence.split(',')
-        else:
-            sentences.remove(sentence)
-    return sentences
+class tokenizer: 
+    def sentence_split(self, text):
+        sentences = text.split('.')
+        count = 0
+        
+        sentences.remove('') #This line removes the empty values from the list.
+        print('\nTokenizer:\n'+text)
+        print('sentences('+str(len(sentences))+') : ')
+        for sentence in sentences:
+            if (sentence != ''):
+                print(' -> '+sentence)
+                sentence.split(',')
+            else:
+                sentences.remove(sentence)
+        return sentences
 
 
-#This function splits sentences into words and stores them as a list.
-def word_split(text):
-    sentences_list = []
-    count = 0
-    total_words = 0
-    for sentence in text:
-        count += 1
-        words = sentence.split(' ')
+    #This function splits sentences into words and stores them as a list.
+    def word_split(self, text):
+        sentences_list = []
+        count = 0
+        total_words = 0
+        for sentence in text:
+            count += 1
+            words = sentence.split(' ')
 
-        print('\nSentence ('+str(count)+') :\n'+ sentence)
-        print('Words:'+'('+str(len(words))+')')
+            print('\nSentence ('+str(count)+') :\n'+ sentence)
+            print('Words:'+'('+str(len(words))+')')
 
-        total_words += len(words);
+            total_words += len(words);
 
-        for word in words:
-            #Removing Commas from words
-            #if (word.count(',')>=1):
-             #   word.remove(',')
-            print('-> ' + word)
-        sentences_list.append(words)
+            for word in words:
+                #Removing Commas from words
+
+                char = word.split()
+                if (char.count(',')>=1):
+                    char.remove(',')
+                word = ''.join(char)            
+                print('-> ' + word+' ('+str(len(word))+')')
+            sentences_list.append(words)
+
+            
+        #This segment prints the Statistics of the tokenizer
         print('\nSummary:')
         print('Total Senctences: '+ str(len(sentences_list)))
-        print('Total number of Words: '+str(total_words))
-    return sentences_list
+        print('Total number of Tokens: '+str(total_words)+'\n')
+        return sentences_list
 
 
 
