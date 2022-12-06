6c4be0d04e3d1d7f5333a9c7637e33cc30782f06
Abdoul <azisabdoul75@gmail.com>
2022-12-06 14:00:20 +0100

    ajustment

--- core/test_structure.py
+++ core/test_structure.py
@@ -1,7 +1,7 @@
 import pytest
 import structure
 
-from .structure import Document, sentencize, tokenize
+from structure import Document, sentencize, tokenize
 
 def test_Document_Token_Sentence():
     """
