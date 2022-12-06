10a6968e74f8d712c6dc9920dc2918ac904434d6
FaibeeElisa <97232341+FaibeeElisa@users.noreply.github.com>
2022-12-06 09:15:22 +0100

    module 5 main.js

---
+++ tokeniser/main.js
@@ -0,0 +1,11 @@
+text = "The quick brown fox jumped over the lazy dog. The Main miners of the world. An anthilled"
+//divide into sentences
+var check_sentence = (text)=>{
+    split_sentence = text.split('.')
+    console.log(`sentences(${split_sentence.length}):`);
+    for(i = 0; i < split_sentence.length; i++){
+        console.log('  -> '+split_sentence[i]);
+    }
+return split_sentence
+}
+
