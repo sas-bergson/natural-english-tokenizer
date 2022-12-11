fbf3bb1636c459013b1dbe9cbe7b86bbb627432a
Kila Frederick Kisife <kilafrederick@gmail.com>
2022-12-11 20:14:40 +0100

    deoxygen file comments added

--- main.py
+++ main.py
@@ -1,51 +1,116 @@
-#!/usr/bin/env python3
-##
-# @mainpage Tokenizer Project
-#
-# @section description_main Description
-# The is the generated documentation for group 2's part of the project.
-# generating source code documentation with Doxygen.
-#
-# @section notes_main Notes
-# - The course project is highly experimental and will involve 3 students in this group
-#
-# Copyright (c) 2022 Compiler Construct.  All rights reserved.
-##
-# @file statement.py.py
-#
-# @brief Example Python program with Doxygen style comments.
-#
-# @section description_doxygen_example Description
-# @section author_doxygen_example Author(s)
-# - Created by Kila Frederick Kisife on 12/1/2022.
-# - Modified by Dikuba Kindjel Alicia Ashley on 12/1/2022.
-# Copyright (c) 2022 Compiler construct.  All rights reserved.
-# Imports
+import re
+from parts_speech import *
 
-from statement import statement_tokenizer
+"""! @brief Example Python program with Doxygen style comments."""
 
 
-def main():
-    """! @brief Example Python program with Doxygen style comments."""
+class statement_tokenizer:
+    def __init__(self):
+        """
+        This method is the constructor. where all the regex are defined and the word that the user or the calling file will use to call the method."""
+        self._pattern = r"[A-Z]+[a-z]*\s\."
+        self._sentence_pattern = r'([A-Z][^\.!?]*[\.!?])'
+        self._word_pattern = r'\w+'
+        self._regex = re.compile(self._sentence_pattern)
+        self._tokens = []
+        self.nounList = []
+        self.pronounList = []
+        self.adjectiveList = []
+        self.verbList = []
+        self.averbList = []
+        self.conjunctionsList = []
+        self.interjectionsList = []
+        self.unknownList = []
+        self.adverbList = []
 
+    def get_tokens(self, text) -> list:
+        """
+        This method is responsible for the splitting of the individual strings into the required tokens which
+        takes  text (string) the text that is to be split into tokens. and returns a list of tokens."""
+        self._tokens = self.peformSentenceSplit(text)
+        return self._tokens
 
-    text = "Here comes the men in black. We are working together."
+    def __str__(self) -> str:
+        """
+        This method is responsible for the printing of the tokens that are returned by the get_tokens method.
+        it takes no arguments and returns a string of the tokens. it basically overites the default __str__ method."""
+        for s in self._tokens:
+            print(f"statement -> {s}")
 
-    tokenizer = statement_tokenizer()
-    print("The function of to get all the tokens is: ",
-          tokenizer.getAllTokens(text))
-    print("This is the method is responsible for the token retension:",
-          tokenizer.retainAllTokens(text))
-    print("THIS IS RESPONSIBLE FOR IDENTIFICATION OF VALID SENTENCES AND ")
-    print(tokenizer.peformSentenceSplit(text))
-    print("THIS IS RESPONSIBLE FOR MATHING ALL WORDS STARTING AN A OR a")
-    print(tokenizer.matchAllWordsStartingWithA(text))
-    print("THIS IS A THAT PARTIAL MATCHES THE TEXT TYPES:")
-    words = tokenizer.performWordSplit(text)
+    def __repr__(self) -> str:
+        """
+        This method is responsible for the printing of the tokens that are returned by the get_tokens method.
+        it takes no arguments and returns a string of the tokens. it basically overites the default __repr__ method."""
+        for s in self._tokens:
+            print(f"statement -> {s}")
 
-    # for word in words:
-    #     print(word+tokenizer.fsa(word))
+    def peformSentenceSplit(self, text):
+        """This function takes part in tokenization of whole text blocks to aid the word tokenizer to be able to identifty tokens
+        based on specific word. This is a part which makes the whole system modular making it possible for the sentence to be handled as
+        blocks. this will also give the possibility to thet count of the number of sentences in
+        """
+        formatter = re.compile(self._sentence_pattern, re.M)
+        return formatter.findall(text)
 
+    def performWordSplit(self, text):
+        """This function takes part in tokenization of whole text blocks to aid the word tokenizer to be able to identifty tokens
+        based on specific word. This is a part which makes the whole system modular making it possible for the sentence to be handled as
+        blocks. this will also give the possibility to thet count of the number of sentences in
+        """
+        formatter = re.compile(self._word_pattern, re.M)
+        return formatter.findall(text)
 
-if __name__ == "__main__":
-    main()
+    def getAllTokens(self, text):
+        """
+        This function is responsible for the tokenization of the whole text block. it takes the text block as an argument and returns a list of tokens.
+        """
+        textData = self.retainAllTokens(text)
+        return textData.split()
+
+    def retainAllTokens(self, text):
+        """
+        this fuction is tp prevent the elimination of special characters to avoid elimination during text splitting
+        this will be especially important what there will to be identification of known patters.
+        The function contains a special regular expresssion that checks all characaters individually
+        """
+        new_text = ""
+        for i in range(len(text)):
+            if re.match(r'\.|,|\?|\'|\)|\}|\]|\:|\;', text[i]):
+                new_text = new_text+" "+text[i]
+            elif re.match(r'\(|\{|\[|\s', text[i]):
+                new_text = new_text+text[i]+" "
+            else:
+                new_text = new_text+text[i]
+        return new_text
+#    generate a dunction to match all words starting with an A or a
+
+    def matchAllWordsStartingWithB(self, text):
+        """The aim of this function is to match all words starting with an A or a this is performed by the use of a regular expression"""
+        formatter = re.compile(r'\b[Bb]\w+', re.M)
+        data = re.findall(formatter, text)
+        print('Words starting with B or b are: ', len(data))
+        return data
+    def identifyPartsOfSpeech(self, text):
+        """This function is responsible for the identification of the parts of speech of the words in the text block.
+        it takes the list of words as an argument and returns a list of parts of speech. This will make use of the [parts_speech] file
+        which contains the list of parts of speech and the words that belong to each part of speech. The group works on code that deals with the letter B
+        In cases where the
+        """
+        data = self.matchAllWordsStartingWithB(text)
+        for word in data:
+            if word in nouns:
+                self.nounList.append(word)
+            elif word in pronouns:
+                self.pronounList.append(word)
+            elif word in adjectives:
+                self.adjectiveList.append(word)
+            elif word in verbs:
+                self.verbList.append(word)
+            elif word in adverbs:
+                self.adverbList.append(word)
+            elif word in conjunctions:
+                self.conjunctionsList.append(word)
+            elif word in interjections:
+                self.interjectionsList.append(word)
+            else:
+                self.unknownList.append(word+" Unknown")
