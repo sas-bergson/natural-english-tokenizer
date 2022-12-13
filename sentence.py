f7076a947984841753f75b1f1094867f386528cf
Emmanueljr07 <116388126+Emmanueljr07@users.noreply.github.com>
2022-12-13 15:38:54 +0100

    Dictionary of words

--- sentence.py
+++ sentence.py
@@ -12,10 +12,20 @@ class Sentence:
         self.word_pattern = re.compile(r'[a-zA-Z0-9]+')
         self._adverbs = r"\w+ly\b"
         self._verbs = r"\w+ing\b"
+        
+    def Sentencesplit(self, text):
+        '''This function should split the sentence'''
+        
+        # splitted = re.split('\.', text)
+        # print(splitted)
+        matches = re.findall('[A-Z][^\.!?]*[!?\.]', text)
+        new_match = []
+        for i in matches:
+            print(i+1)
+        
     
     def sentence_check(self, text):
         '''This function checks if the text is a proper sentence'''
-        
         checked = re.findall(self._sentence_pattern, text)
         for word in checked:
             print(word)
@@ -23,11 +33,9 @@ class Sentence:
         return word
         
     def get_token(self, text) -> list :
-        '''This functions splits the sentence into words'''
-        
+        '''This functions splits the sentence into words'''  
         # tokens = re.split(self._pattern, text)
         tokens = text.split('.')
-        
         for word in tokens:
             print(word)
         # print(tokens)
@@ -35,38 +43,27 @@ class Sentence:
     
     def words_withV(self, text):
         '''Matching words that begin with V and printing them'''
-        
         x = re.findall(self._v_pattern, text)
         print(x)
         
     def matchtextlength(self, text):
         '''This function returns the length of all the words in this sentence'''
-        
         for x in range(len(text)):
             print("".join([" ".join(text[x]), " (length = ", str(len(text[x])), ")"] ))
             # print(len(text[x]))
-            
+
         # words = re.findall(self.word_pattern, text)
         # length = len(re.findall(self.l_pattern, text))
-        
         # print("".join([" ".join(words),"   (length = ", str(length), ")"]))
         # return length
-    
-    def sentencesplit(self, text):
-        '''Splits each sentence in the sentence'''
-        splitted = re.split(self._space_split, text)
-        print(splitted)
 
     def findalladverbs(self, text):
-        '''This functions displays all the adverbs in a text and their position'''
-        
+        '''This functions displays all the adverbs in a text and their position''' 
         for m in re.finditer(self._adverbs, text):
             print('Adverb: '+'%02d-%02d: %s' % (m.start(), m.end(), m.group(0)))
     
     def findallverbs(self, text):
         '''This functions displays all the adverbs in a text and their position'''
-        
         for m in re.finditer(self._verbs, text):
             print( 'verb:'+'%02d-%02d: %s' % (m.start(), m.end(), m.group(0)))
    
-    
---
+++ wordswithV.py
@@ -0,0 +1,38 @@
+nouns = {
+'Vaccaria': 'noun', 'Vacancy': 'noun', 'Vacation': 'noun', 'Vacationer': 'noun','Vacationist': 'noun', 
+'Vacationland': 
+'noun', 'Vacay': 'noun', 'Vaccina': 'noun', 'Vaccination': 'noun', 'Vaccinator': 'noun', 'Vaccine': 'noun',
+'Vaccinee':'noun', 'Vaccinia': 'noun', 'Vaccinium': 'noun', 'Vacuolization': 'noun', 'Vacuum': 'noun',
+'Vagabond': 'noun', 'Vagina': 'noun', 'Vaginal': 'noun', 'Valet': 'noun', 'Validation': 'noun', 'Validity': 'noun',
+'Valley': 'noun', 'Valor': 'noun', 'Valuation': 'noun', 'Value': 'noun', 'Valve': 'noun', 'Van': 'noun',
+'Vandalism': 'noun','Vanguard': 'noun', 'Vanilla': 'noun', 'Vanity': 'noun', 'Vantage': 'noun', 'Vapor': 'noun',
+'Vaporization': 'noun', 'Vaquero': 'noun', 'Variability': 'noun', 'Variable': 'noun', 'Variance': 'noun',
+'Variant': 'noun', 'Variation': 'noun', 'Variety': 'noun', 'Varmint': 'noun', 'Varnish': 'noun', 'Vasa': 'noun',
+'Vase': 'noun', 'Vasorum': 'noun', 'Vassal': 'noun', 'Vaudeville': 'noun', 'Vault': 'noun',
+'Vaulting': 'noun', 'Veadar': 'noun', 'Veal': 'noun', 'Veblen': 'noun', 'Vector': 'noun', 'Veda': 'noun',
+'Vedalia': 'noun', 'Vedanga': 'noun', 'Vedanta': 'noun', 'Vedism': 'noun', 'Vedist': 'noun', 'Veery': 'noun',
+'Vega': 'noun', 'Vegan': 'noun', 'Vegetable': 'noun', 'Vegetarian': 'noun', 'Vegetation': 'noun',
+'Vehemence': 'noun', 'Vehicle': 'noun', 'Veil': 'noun', 'Vein': 'noun', 'Veldt': 'noun', 'Vellum': 'noun',
+'Velocity': 'noun', 'Velour': 'noun', 'Velvet': 'noun', 'Vendor': 'noun', 'Veneer': 'noun', 'Veneration': 'noun',
+'Vengeance': 'noun', 'Venison': 'noun', 'Venom': 'noun', 'Vent': 'noun', 'Ventilation': 'noun',
+'Ventilator': 'noun', 'Ventricle': 'noun', 'Venture': 'noun', 'Veracity': 'noun', 'Veranda': 'noun',
+'Verandah': 'noun', 'Verb': 'noun', 'Verdict': 'noun', 'Verge': 'noun', 'Verification': 'noun',
+'Verisimilitude': 'noun', 'Verity': 'noun', 'Vermilion': 'noun', 'Vermouth': 'noun', 'Vernacular': 'noun',
+'Vernier': 'noun', 'Veronica': 'noun', 'Versatility': 'noun', 'Verse': 'noun', 'Version': 'noun',
+'Vertebrae': 'noun', 'Vertebrate': 'noun', 'Vertex': 'noun', 'Vertigo': 'noun', 'Verve': 'noun', 'Vessel': 'noun',
+'Vest': 'noun', 'Vestibule': 'noun',
+}
+
+Verbs = {
+'vacate': 'verb', 'vaccinate': 'verb', 'validate': 'verb', 'value': 'verb',	'vamp': 'verb', 'variegate': 'verb',
+'varnish': 'verb', 'vary': 'verb', 'vaticinate': 'verb', 'vault': 'verb', 'vegetate': 'verb', 'veil': 'verb',
+'vein': 'verb', 'veneer': 'verb','venerate': 'verb', 'ventilated': 'verb', 'ventured': 'verb', 'verified': 'verb', 	
+'vermilioned': 'verb', 'versify': 'verb', 'vetoed': 'verb', 'vexed': 'verb', 'vibrated': 'verb','victimize': 'verb',
+'view': 'verb', 'vindicated': 'verb', 'violate': 'verb','visit': 'verb', 'visualized': 'verb', 'visualized': 'verb', 	
+'vitrify': 'verb', 'vitriolize': 'verb', 'vocalize': 'verb', 'vociferate': 'verb', 'voiced': 'verb', 'volatilized': 'verb',
+'vomit': 'verb', 'voted': 'verb', 'vowed': 'verb', 'vulcanized': 'verb', 'vulgarized': 'verb'
+}
+
+Aderbs = {
+    
+}
